3fdf0d02e61f0c4fd7afbd01c9020ebb62cd30cb
Luis Alberto <lalberto8085@gmail.com>
2015-02-11 14:06:22 -0500

    some minor improvements, duplication in sorted lists needed

--- Tutorial1/HackerRank/messyMedian2.fsx
+++ Tutorial1/HackerRank/messyMedian2.fsx
@@ -5,7 +5,8 @@ let calculateNewValuesAndMedian (minValues : SortedSet<int>) (maxValues : Sorted
     if(minValues.Count = 0) then
         minValues.Add(case) |> ignore
     else
-        if case > maxValues.Min then // adding to max
+//        printfn "max.min: %i; case: %i" maxValues.Min case
+        if case > minValues.Max then // adding to max
             if maxValues.Count > minValues.Count then
                 minValues.Add(maxValues.Min) |> ignore
                 maxValues.Remove(maxValues.Min) |> ignore
@@ -20,23 +21,26 @@ let calculateNewValuesAndMedian (minValues : SortedSet<int>) (maxValues : Sorted
                     else minValues.Max
     minValues, maxValues, median
 
-let updateCache (cache: Dictionary<int, int[] * int[] * int>) (jumpIndexes : Queue<int>)(minValues : SortedSet<int>) (maxValues : SortedSet<int>) median index =
+let updateCache (cache: Dictionary<int, int list * int list * int>) (jumpIndexes : Queue<int>)(minValues : SortedSet<int>) (maxValues : SortedSet<int>) median index =
     if jumpIndexes.Count > 0 && jumpIndexes.Peek() = index then
         let mutable minArr = Array.init minValues.Count (fun _ -> 0)
         minValues.CopyTo(minArr)
         let mutable maxArr = Array.init maxValues.Count (fun _ -> 0)
         maxValues.CopyTo(maxArr)
-        cache.Add(jumpIndexes.Dequeue(), (minArr, maxArr, median))
+        cache.Add(jumpIndexes.Dequeue(), (minArr |> Array.toList, maxArr |> Array.toList, median))
 
 let calculateMedianAndValues cache jumpIndexes minValues maxValues index case = 
     if case > 0 then
         let newMins, newMaxs, median = calculateNewValuesAndMedian minValues maxValues case
         updateCache cache jumpIndexes newMins newMaxs median index
+        printfn "min: %A; max: %A; median: %i; case: %i; index: %i" newMins newMaxs median case index
         median, newMins, newMaxs
     else
         let minsArr, maxArr, median = cache.[index+case]
         let newMins = new SortedSet<int>(minsArr)
         let newMaxs = new SortedSet<int>(maxArr)
+        updateCache cache jumpIndexes newMins newMaxs median index
+        printfn "min: %A; max: %A; median: %i; case: %i; index: %i" newMins newMaxs median case index
         median, newMins, newMaxs
 
 let rec solve cache cases jumpIndexes results minValues maxValues index =
@@ -64,12 +68,7 @@ let cases = [19; 19;-1; 18; 19; 17; -6; 18; -1; -5; -4; 16; 17; 17; 16; 19; 20; 
 //let caseCount = int <| Console.ReadLine()
 //let cases = [1..caseCount] |> List.map (fun _ -> int <| Console.ReadLine())
 
-let cache = new Dictionary<int, int[] * int[] * int>()
-
-let folder (queue :Queue<int>) item =
-    if not <| queue.Contains(item) then
-        queue.Enqueue(item)
-    queue
+let cache = new Dictionary<int, int list * int list * int>()
 
 // indexes where there is a jump-back 
 let jumpBacks = cases |> Seq.mapi (fun i x -> i, x) 
