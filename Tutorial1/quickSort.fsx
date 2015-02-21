595fc431f6c6229150de5d0214ed415573af4e88
Orlando William <0rlandw@gmail.com>
2015-02-20 19:18:32 -0500

    hello pull-requests?

--- Tutorial1/quickSort.fsx
+++ Tutorial1/quickSort.fsx
@@ -4,6 +4,6 @@ let rec quicksort = function
     | [] -> []
     | first :: rest ->
         let smaller, larger = List.partition ((>=) first) rest
-        List.concat [quicksort smaller; [first]; quicksort larger]
+        quicksort smaller @ [first] @ quicksort larger
 
 printfn "%A" (quicksort [1;5;23;18;9;1;3])
